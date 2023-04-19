af30c67463ee6110b7a9d30aa6bc17818e72b9bb
WighNo <65411397+WighNo@users.noreply.github.com>
2023-04-19 19:41:30 +0500

    Add about me

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,15 +1,34 @@
 <script setup>
 import Image from 'primevue/image';
+import Divider from "primevue/divider";
 
+const abilities = [
+    'Опыт работы: ТИНГ, ~7 месяцев',
+    'Занимался разработкой игр на Unity',
+    'Создавал telegram-ботов на Python и C#',
+    'Писал Backend на ASP.NET',
+    'Разрабатывал ботов на ASP.NET и Python',
+    'Дорабатывал API на Python (FastAPI)',
+    'Vue 3 frontend'
+] 
 </script>
 
 <template>
-  <div class="grid">
-      <div class="col"></div>
-      <div class="text-center">
-          <Image class="mt-5 border-round-3xl" :src="require(`@/assets/photo.jpg`)" alt="Image" width="250" preview />
-          <div class="text-4xl font-light text-800">Рудных Павел</div>
-      </div>
-      <div class="col"></div>
-  </div>
+    <div class="grid">
+        <div class="col"></div>
+        <div class="text-center">
+            <Image class="mt-5 border-round-3xl" :src="require(`@/assets/photo.jpg`)" alt="Image" width="250" preview />
+            <div class="text-4xl font-light text-800">Рудных Павел</div>
+            <div class="text-500 text-sm">Хочу быть как Мэри Сью</div>
+        </div>
+        <div class="col"></div>
+    </div>
+    <div class="flex justify-content-center">
+        <Divider class="text-center w-30rem"/>
+    </div>
+    <div class="text-center">
+        <div v-for="ability in abilities" :key="ability" class="text-2xl mt-5">
+            {{ability}}
+        </div>
+    </div>
 </template>
